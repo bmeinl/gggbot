423d56b5a726b904ea4836c5a413ee1fbdd0d2d2
Benjamin Meinl <b.meinl@gmail.com>
2009-12-18 02:05:14 +0100

    first commit

---
+++ gggbot.rb
@@ -0,0 +1,66 @@
+require 'eventmachine'
+
+module Bot
+    Nick = 'InspectorGadget'
+    # just so I don't forget to remove the password when
+    #+I'm uploading this file somewhere ...
+    Password = IO.read('password')
+    Ident = 'InspectorGadget'
+    Realname = 'Dr. Dr. I. Gadget'
+    Perform = '##off-archlinux'
+    Admins = %w/ben_m brandeis/
+
+    def parse_msg(who, s)
+        channel, message = s.scan(/(\S+) :(.+)/).first
+
+        # admin only commands go here
+        if Admins.include?(who)
+            case message
+            when '%quit'
+                send_data "QUIT :Yes, my master!\r\n"
+            when /^%join (\S+)/
+                send_data "JOIN #{$1}\r\n"
+            end
+        end
+
+        # regular commands go here
+        case message
+        when '%version'
+            echo channel, 'Version 0.awesome'
+        when '%wishlist'
+            echo channel, 'http://etherpad.com/bgQFCJRloy'
+        when '%admins'
+            echo channel, Admins * ', '
+        end
+    end
+
+    def echo(channel, message)
+        send_data "PRIVMSG #{channel} :#{message}\r\n"
+    end
+
+    def receive_data(data)
+        $stderr.puts data
+        # who: who sent the message
+        # what: what type of message (PRIVMSG, PING, ...)
+        # rest: rest of message, depends on 'what'
+        who, what, rest = data.scan(/:([^!]+)\S+ (\S+) (.+)\r\n/).first
+
+        case what
+        when "PING"
+            send_data "PONG #{rest}\r\n"
+        when "PRIVMSG"
+            parse_msg who, rest
+        end
+    end
+
+    def post_init
+        send_data "NICK #{Nick}\r\n"
+        send_data "USER #{Ident} bla bla :#{Realname}\r\n"
+        send_data "PRIVMSG NickServ :identify ben_m #{Password}\r\n"
+        send_data "JOIN #{Perform}\r\n"
+    end
+end
+
+EM.run do
+    EM.connect 'irc.freenode.org', 6667, Bot
+end
